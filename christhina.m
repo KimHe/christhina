6eec542fd48ce280a73160ef6efc2d361c7e3260
KimHe <kingdomhe@163.com>
2019-09-30 14:22:18 +0800

    [misc:] structure change

--- christhina.m
+++ christhina.m
@@ -1,54 +1,16 @@
-function christhin
+function christhina
 % ================================================================== %
-% CHRISTHINA: Chromatography RIser THIN Analysis
+% CHRISTHINA: CHromatography RISer THIN Analysis
 % ================================================================== %
 
 
     j = 0;
 
-    choice = 'No';
+    iterative = 'Yes';
 
-    [fileName, pathName, ~] = uigetfile({'*.jpg','JPG (*.jpg)'; ...
-        '*.jpeg','JPEG (*.jpeg)'; '*.bmp','BMP Bitmaps'; ...
-        '*.tiff','TIFF (*.tiff)'; '*.tif','TIF (*.tif)'; ...
-        '*.png','PNG (*.png)'; ...
-        '*.*',  'All files (*.*)'}, 'Select a image', 'MultiSelect', 'off');
+    imgTLC = imgPreProcess;
 
-    [~, folderName, ~] = fileparts(fileName);
-
-    cd(pathName);
-    mkdir(folderName);
-
-    figure(1);
-    imgRaw = rgb2gray(imread(fileName));
-    imshow(imgRaw);
-
-    opts.Interpreter = 'latex';
-    opts.Default = 'OK';
-
-    while strcmp(choice,'No') == 1
-
-        rotateAngle = inputdlg({'Input the angle ($$\theta$$) to rotate the image'}, ...
-            'Theta Value', [1, 60], {'0'}, opts);
-
-        if str2double(rotateAngle{1}) ~= 0
-            imgTLC = imrotate(imgRaw, str2double(cell2mat(rotateAngle)));
-        else
-            imgTLC = imgRaw;
-        end
-
-        imshow(imgTLC);
-
-        choice = questdlg('Is the rotation angle correct?', 'Theta Value', 'OK', 'No', opts);
-
-    end
-
-    cd(folderName);
-
-    imwrite(imgTLC, 'TLCgs.bmp', 'bmp')
-
-
-    while strcmp(choice, 'OK') == 1
+    while strcmp(iterative, 'Yes') == 1
 
         j = j + 1;
 
@@ -56,12 +18,15 @@ function christhin
 
         imshow(imgCrop);
 
-        [row, ~] = size(imgCrop);
         [~, seedFront] = ginput(2);
+
         close(gcf);
 
         if seedFront(1) <= 1; seedFront(1) = 1; end
-        if seedFront(2) >= row; seedFront(2) = row-1; end
+
+        if seedFront(2) >= size(imgCrop)
+            seedFront(2) = size(imgCrop) - 1;
+        end
 
         seedFront = floor(seedFront);
 
@@ -73,27 +38,7 @@ function christhin
 
         grayScale = mean(imgCropInv.');
 
-        figure(2);
-        box on;
-        grid on;
-        hold all;
-        plot(pixels, grayScale, 'k', 'LineWidth', 2, 'MarkerSize', 15, 'MarkerFaceColor', 'w');
-        set(2, 'Color', 'w');
-        set(gca, 'FontSize', 20);
-        xlabel('Pixels', 'FontSize', 20);
-        ylabel('Mean Inverse Grayscale', 'FontSize', 20);
-        axis([0 max(pixels) 0 max(grayScale)+10]);
-
-        questdlg('Please select even number of points','Tips');
-
-        [peakGrid, ~] = ginput;
-
-        numPeakGrid = length(peakGrid);
-        if mod(numPeakGrid, 2) ~= 0
-            error('PlseedFrontYease select even number of points');
-        else
-            peakNum = numPeakGrid/2;
-        end
+        [peakNum, peakGrid] = imgPlotPick(pixels, grayScale);
 
 
         areas = zeros(1, peakNum);
@@ -106,17 +51,17 @@ function christhin
 
             range = find( pixels < peakGrid(k+1) & pixels > peakGrid(k) );
 
-            x1 = pixels(range);
+            x = pixels(range);
 
-            q = grayScale(range);
+            y = grayScale(range);
 
-            grad = ((q(1) - q(end)) / (peakGrid(k) - peakGrid(k+1)));
+            grad = ((y(1) - y(end)) / (peakGrid(k) - peakGrid(k+1)));
 
-            ord = q(end) - (grad * peakGrid(k+1));
+            ord = y(end) - (grad * peakGrid(k+1));
 
-            y1 = grad * x1 + ord;
+            y1 = grad * x + ord;
 
-            areas(i) = trapz(x1, q) - trapz(x1, y1);
+            areas(i) = trapz(x, y) - trapz(x, y1);
 
             [yMax, idx] = max(grayScale(range));
 
@@ -130,57 +75,150 @@ function christhin
 
         end
 
-
-        chroma = [pixels, grayScale];
-
-        str = sprintf(['chromatogram' num2str(j) '.dat']);
-        save(str, '-ascii', '-append', '-double', '-tabs');
-
-        print('-f2','-dpdf','chromatogram');
-
-        movefile('chromatogram.pdf', ['chromatogram' num2str(j) '.pdf']);
-
-        close all
-
         areaTotal = sum(areas);
 
         areaRatio = (areas / areaTotal) * 100;
 
         output = [peakIdx; areaRatio; retentVal]';
 
-        %msgbox(num2str(R), ['Report ' num2str(j)],'none')
+        dataWrite(j, pixels, grayScale, output);
 
-
-        TM = round(clock);
-
-        comment = inputdlg('Input a comment','Notes');
-        mstxt = ['\n\n' 'Comment: ' cell2mat(comment) '\n'];
-        mstxt = [mstxt 'Date: ' num2str(date) '\n'];
-        mstxt = [mstxt 'Time: ' num2str(TM(4)) ':' num2str(TM(5)) '\n'];
-        mstxt = [mstxt 'Chosen figure: ' fileName '\n' ] ;
-        mstxt = [mstxt 'Processed figure: TLCgs.bmp \n'];
-        mstxt = [mstxt 'Run: TLCRun' num2str(j) '.bmp \n'];
-        mstxt = [mstxt 'Converted to chromatogram' num2str(j) '.pdf \n'];
-        mstxt = [mstxt 'Pick\tArea \t\tRf \n' ];
-
-        [r, c] = size(output);
-        for k = 1:r
-            for l = 1:c
-                mstxt = [mstxt num2str(output(k, l)) '\t\t'];
-            end
-            mstxt = [mstxt '\n'];
-        end
-
-        fid = fopen(['output' num2str(j) '.dat'], 'a');
-        fprintf(fid,['\n ' mstxt]);
-        fclose(fid);
-
-        choice = questdlg('Do you want to realize another run?', ...
+        iterative = questdlg('Do you want to realize another run?', ...
             'TLC analysis','OK','No',opts);
 
     end
 
     cd('..')
+
+end
+
+
+function imgTLC = imgPreProcess
+% ================================================================== %
+% Read rotate and show the loaded image
+% ================================================================== %
+
+
+    global fileName;
+
+    rotate = 'No';
+
+    fileName = uigetfile({'*.jpg','JPG (*.jpg)'; '*.jpeg','JPEG (*.jpeg)';
+    '*.bmp','BMP Bitmaps'; '*.tiff','TIFF (*.tiff)'; '*.tif','TIF (*.tif)'; ...
+        '*.png','PNG (*.png)'; '*.*', 'All files (*.*)'}, ...
+        'Select a image', 'MultiSelect', 'off');
+
+    [~, folderName, ~] = fileparts(fileName);
+
+    if exist(folderName, 'dir') ~= 7
+        mkdir(folderName);
+    end
+
+    figure(1);
+
+    imgRaw = rgb2gray( imread(fileName) );
+
+    imshow(imgRaw);
+
+    opts.Interpreter = 'latex';
+    opts.Default = 'OK';
+
+    while strcmp(rotate, 'No') == 1
+
+        rotateAngle = inputdlg({'Input the angle ($$\theta$$) to rotate the image'}, ...
+            'Theta Value', [1, 60], {'0'}, opts);
+
+        if str2double(rotateAngle) ~= 0
+            imgTLC = imrotate(imgRaw, str2double(cell2mat(rotateAngle)));
+        else
+            imgTLC = imgRaw;
+        end
+
+        imshow(imgTLC);
+
+        rotate = questdlg('Is the rotation angle correct?', 'Theta Value', 'Yes', 'No', opts);
+
+    end
+
+    imgTLC = imgRaw;
+
+    cd(folderName);
+
+    imwrite(imgTLC, 'TLCgs.bmp', 'bmp')
+
+end
+
+
+function [peakNum, peakGrid] = imgPlotPick(pixels, grayScale)
+% ================================================================== %
+% Plot chromatogram and pick up the peaks to be analyzed
+% ================================================================== %
+
+
+    figure(2);
+
+    box on;
+    grid on;
+    hold all;
+
+    plot(pixels, grayScale, 'k', 'LineWidth', 2, 'MarkerSize', 15, 'MarkerFaceColor', 'w');
+
+    axis([0 max(pixels) 0 max(grayScale)+10]);
+
+    set(2, 'Color', 'w');
+    set(gca, 'FontSize', 20);
+
+    xlabel('Pixels', 'FontSize', 20);
+    ylabel('Mean Inverse Grayscale', 'FontSize', 20);
+
+
+    questdlg('Please select even number of points', 'Tips');
+
+    [peakGrid, ~] = ginput;
+
+    numPeakGrid = length(peakGrid);
+
+    if mod(numPeakGrid, 2) ~= 0
+        error('Plsease select even number of points');
+    else
+        peakNum = numPeakGrid/2;
+    end
+
+end
+
+
+function dataWrite(j, pixels, grayScale, output)
+% ================================================================== %
+% Write out the chromatogram and log file
+% ================================================================== %
+
+
+    global fileName;
+
+    chroma = [pixels, grayScale];
+
+    str = sprintf(['chromatogram' num2str(j) '.dat']);
+    save(str, '-ascii', '-append', '-double', '-tabs');
+
+    print('-f2','-dpdf','chromatogram');
+    movefile('chromatogram.pdf', ['chromatogram' num2str(j) '.pdf']);
+
+    close all
+
+
+    TM = round(clock);
+
+    comment = inputdlg('Input a comment','Notes');
+
+    strOut = sprintf('Comment: %s \n Date: %s \n Time: %s : %s \n Chosen figure: %s \n Processed figure TLCgs.bmp \n Run: TLCRun%s.bmp \n Converted to chromatogram%s.pdf \n Pick\tArea \t\tRetention value \n',...
+        cell2mat(comment), num2str(date), num2str(TM(4)), num2str(TM(5)), fileName, num2str(j), num2str(j));
+
+    fid = fopen(['output' num2str(j) '.dat'], 'a');
+    fprintf(fid,['\n ' strOut]);
+
+    fclose(fid);
+
+    save(sprintf('output%s.dat', num2str(j)), 'output', '-ascii', '-append', '-tabs');
 
 end
 % ================================================================== %
